ba0091fb012bfdf948b9296bdacc8c72f058b41b
Kristof Balasko <kristof@balaskokristof.hu>
2024-03-08 10:49:03 +0100

    Todo adatstruktúra definiálása a Models mappába

---
+++ Models/Todo.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    protected $table = 'todos'; // SQLite adatbázis táblának a neve
+    protected $fillable = ['title', 'description', 'completed']; //a Todo adatszerkezete
+}
+
+//Forrás: https://www.slimframework.com/docs/v3/tutorial/first-app.html 2024.03.08 10:46 - Slim dokumentáció
